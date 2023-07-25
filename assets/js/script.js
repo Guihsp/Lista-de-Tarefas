3a80a5a043b0e0ff755d6c0c6fcabb1f752c19af
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2023-07-24 22:25:13 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+const inputTask = document.querySelector(`#input-task`);
+const btnAddTask = document.querySelector(`#btn-add`);
+const taskList = document.querySelector(`#task-list`);
+
+function createLi() {
+    const li = document.createElement(`li`);
+    return li;
+}
+
+inputTask.addEventListener(`keypress`, function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTask.value) return;
+        createTask(inputTask.value);
+    }
+});
+
+function cleanInput() {
+    inputTask.value = ``;
+    inputTask.focus();
+}
+
+function createDeleteButton(li) {
+    const deleteButton = document.createElement(`button`);
+    deleteButton.innerText = `Apagar`;
+    deleteButton.setAttribute(`class`, `btn-delete`);
+    deleteButton.setAttribute(`title`, `Delete button`);
+    li.appendChild(deleteButton);
+}
+
+document.addEventListener(`click`, function (event) {
+    const element = event.target;
+
+    if (element.classList.contains(`btn-delete`)) {
+        element.parentElement.remove();
+        saveTask()
+    }
+})
+
+function createTask(textInput) {
+    const li = createLi();
+    li.innerHTML = textInput;
+    taskList.appendChild(li);
+    cleanInput();
+    createDeleteButton(li);
+    saveTask();
+}
+
+function saveTask() {
+    const liTasks = taskList.querySelectorAll(`li`);
+    const taskItens = [];
+
+    for (const task of liTasks) {
+        let textTask = task.innerText;
+        textTask = textTask.replace(`Apagar`, ``);
+        taskItens.push(textTask);
+    }
+
+    const taskJSON = JSON.stringify(taskItens);
+    localStorage.setItem(`tasks`, taskJSON);
+}
+
+btnAddTask.addEventListener(`click`, function () {
+    if (!inputTask.value) return;
+    createTask(inputTask.value);
+});
+
+function addSaveTask() {
+    const task = localStorage.getItem(`tasks`);
+    const taskList = JSON.parse(task);
+
+    for (let task of taskList) {
+        createTask(task);
+    }
+}
+addSaveTask();
